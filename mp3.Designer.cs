a7f322810552ea20f7443c80f2f703d365ea0d25
freezecyber <37415540+freezecyber@users.noreply.github.com>
2018-06-11 16:39:07 +0000

    Add files via upload

---
+++ mp3.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace lecteur_mp3
+{
+    partial class mp3
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mp3));
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label14 = new System.Windows.Forms.Label();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.axWindowsMediaPlayer2 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel4.Controls.Add(this.axWindowsMediaPlayer2);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(350, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(375, 236);
+            this.panel4.TabIndex = 31;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(266, 69);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(41, 13);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "label14";
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(98, 12);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(46, 17);
+            this.checkBox8.TabIndex = 12;
+            this.checkBox8.Text = "bmp";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(98, 38);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(37, 17);
+            this.checkBox7.TabIndex = 11;
+            this.checkBox7.Text = "gif";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(186, 12);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(40, 17);
+            this.checkBox6.TabIndex = 10;
+            this.checkBox6.Text = "jpg";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 38);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(40, 17);
+            this.checkBox3.TabIndex = 9;
+            this.checkBox3.Text = "ico";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 12);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(46, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "mp3";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(186, 38);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(40, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "avi";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button16.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button16.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button16.ForeColor = System.Drawing.Color.Red;
+            this.button16.Location = new System.Drawing.Point(269, 90);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(75, 36);
+            this.button16.TabIndex = 6;
+            this.button16.Text = "play";
+            this.button16.UseVisualStyleBackColor = false;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button18
+            // 
+            this.button18.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button18.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button18.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button18.ForeColor = System.Drawing.Color.Red;
+            this.button18.Location = new System.Drawing.Point(269, 12);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(75, 54);
+            this.button18.TabIndex = 4;
+            this.button18.Text = "return";
+            this.button18.UseVisualStyleBackColor = false;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // button17
+            // 
+            this.button17.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button17.ForeColor = System.Drawing.Color.White;
+            this.button17.Location = new System.Drawing.Point(16, 170);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(305, 39);
+            this.button17.TabIndex = 3;
+            this.button17.Text = "null";
+            this.button17.UseVisualStyleBackColor = false;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.Green;
+            this.button15.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button15.Location = new System.Drawing.Point(12, 55);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(214, 71);
+            this.button15.TabIndex = 1;
+            this.button15.Text = "load";
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(244, 132);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 32;
+            this.textBox1.Text = "png";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(161, 135);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(77, 17);
+            this.checkBox4.TabIndex = 33;
+            this.checkBox4.Text = "set by user";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(0, 215);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(350, 21);
+            this.comboBox1.TabIndex = 34;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // axWindowsMediaPlayer2
+            // 
+            this.axWindowsMediaPlayer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axWindowsMediaPlayer2.Enabled = true;
+            this.axWindowsMediaPlayer2.Location = new System.Drawing.Point(0, 0);
+            this.axWindowsMediaPlayer2.Name = "axWindowsMediaPlayer2";
+            this.axWindowsMediaPlayer2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer2.OcxState")));
+            this.axWindowsMediaPlayer2.Size = new System.Drawing.Size(375, 236);
+            this.axWindowsMediaPlayer2.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 35;
+            this.button1.Text = "single";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mp3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::lecteur_mp3.Properties.Resources.Music01;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(725, 236);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.DoubleBuffered = true;
+            this.Name = "mp3";
+            this.Text = "Form1";
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel4;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer2;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ mp3.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lecteur_mp3
+{
+    public partial class mp3 : Form
+    {
+        WMPLib.IWMPPlaylist playlist;
+        WMPLib.IWMPMedia media;
+        private int sing;
+
+        public mp3()
+        {
+            InitializeComponent();
+            playlist = axWindowsMediaPlayer2.playlistCollection.newPlaylist("myplaylist");
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (checkBox4.Checked)
+                {
+                    string[] fts12 = Directory.GetFiles(folderBrowserDialog1.SelectedPath.ToString(), "*."+textBox1.Text, SearchOption.AllDirectories);
+
+                    foreach (string file2 in fts12)
+                    {
+                        comboBox1.Items.Add(file2);
+                        sing++;
+                        media = axWindowsMediaPlayer2.newMedia(file2);
+                        playlist.appendItem(media);
+                    }
+                }
+                if (checkBox1.Checked)
+                {
+                    string[] fts1 = Directory.GetFiles(folderBrowserDialog1.SelectedPath.ToString(), "*.avi", SearchOption.AllDirectories);
+
+                    foreach (string file in fts1)
+                    {
+                        comboBox1.Items.Add(file);
+                        sing++;
+                        media = axWindowsMediaPlayer2.newMedia(file);
+                        playlist.appendItem(media);
+                    }
+                }
+                else
+                {
+                    if (checkBox2.Checked)
+                    {
+                        string[] fts = Directory.GetFiles(folderBrowserDialog1.SelectedPath.ToString(), "*.mp3", SearchOption.AllDirectories);
+
+                        foreach (string file in fts)
+                        {
+                            comboBox1.Items.Add(file);
+                            sing++;
+                            media = axWindowsMediaPlayer2.newMedia(file);
+                            playlist.appendItem(media);
+                        }
+                    }
+                    else
+                    {
+                        if (checkBox8.Checked)
+                        {
+                            string[] fts2 = Directory.GetFiles(folderBrowserDialog1.SelectedPath.ToString(), "*.bmp", SearchOption.AllDirectories);
+
+                            foreach (string file in fts2)
+                            {
+                                comboBox1.Items.Add(file);
+                                sing++;
+                                media = axWindowsMediaPlayer2.newMedia(file);
+                                playlist.appendItem(media);
+                            }
+                        }
+                        else
+                        {
+                            if (checkBox6.Checked)
+                            {
+                                string[] fts3 = Directory.GetFiles(folderBrowserDialog1.SelectedPath.ToString(), "*.jpg", SearchOption.AllDirectories);
+
+                                foreach (string file in fts3)
+                                {
+                                    comboBox1.Items.Add(file);
+                                    sing++;
+                                    media = axWindowsMediaPlayer2.newMedia(file);
+                                    playlist.appendItem(media);
+                                }
+                            }
+                            else
+                            {
+                                if (checkBox7.Checked)
+                                {
+                                    string[] fts4 = Directory.GetFiles(folderBrowserDialog1.SelectedPath.ToString(), "*.gif", SearchOption.AllDirectories);
+
+                                    foreach (string file in fts4)
+                                    {
+                                        comboBox1.Items.Add(file);
+                                        sing++;
+                                        media = axWindowsMediaPlayer2.newMedia(file);
+                                        playlist.appendItem(media);
+                                    }
+                                }
+                                else
+                                {
+                                    if (checkBox3.Checked)
+                                    {
+                                        string[] fts5 = Directory.GetFiles(folderBrowserDialog1.SelectedPath.ToString(), "*.ico", SearchOption.AllDirectories);
+
+                                        foreach (string file in fts5)
+                                        {
+                                            comboBox1.Items.Add(file);
+                                            sing++;
+                                            media = axWindowsMediaPlayer2.newMedia(file);
+                                            playlist.appendItem(media);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                axWindowsMediaPlayer2.currentPlaylist = playlist;
+                axWindowsMediaPlayer2.settings.setMode("shuffle", true);
+                axWindowsMediaPlayer2.Ctlcontrols.play();
+                label14.Text = sing.ToString();
+
+            }
+            else { MessageBox.Show("choose checkbox"); }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+            axWindowsMediaPlayer2.currentPlaylist = playlist;
+            axWindowsMediaPlayer2.settings.setMode("shuffle", true);
+            axWindowsMediaPlayer2.Ctlcontrols.play();
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer2.URL = null;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer2.URL = comboBox1.SelectedItem.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            axWindowsMediaPlayer2.URL = openFileDialog1.FileName.ToString();
+        }
+    }
+}
